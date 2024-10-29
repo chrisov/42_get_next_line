5895d6d3e6ff53839b7f82a6795f4e7f53d7b4ed
dchrysov <j.chrisov@gmail.com>
2024-10-29 01:12:53 +0100

    get_next_line.c: (n/w) Fixed the main code, how the buffer and the remainder are handled, which of them are used to store which data. Leaks are observed, constantly on the 1char test case, regardless the size of the buffer, as well as to other.

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dchrysov <dchrysov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dimitris <dimitris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 13:32:36 by dchrysov          #+#    #+#             */
-/*   Updated: 2024/10/28 18:14:11 by dchrysov         ###   ########.fr       */
+/*   Updated: 2024/10/29 01:08:41 by dimitris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,56 +19,55 @@ char	*get_next_line(int fd)
 	char			*buffer;
 	char			*line;
 	static char		*remainder;
+	char			*delpos;
 
 	if (fd == -1 || BUFFER_SIZE <= 0)
 		return (NULL);
-	buffer = malloc(BUFFER_SIZE + 1);
-	printf("\nbuffer: \"%s\"\n", buffer);
+	buffer = NULL;
 	while (1)
 	{
-		printf("\nremainder: \"%s\"\n", remainder);
-		// if (ft_strncmp(buffer, "", ft_strlen(buffer)) == 0 && remainder != NULL)
-		// 	buffer = remainder;
 		if (remainder)
 		{
-			printf("\nbuffer before: \"%s\"\n", buffer);
-			remainder = ft_strnstr(buffer, "\n", ft_strlen(buffer));
-			remainder++;
-			line = ft_strndup(buffer, ft_strlen(buffer) - ft_strlen(remainder));
-			// ft_memmove(buffer, remainder + 1, ft_strlen(remainder - 2));
-			// buffer[ft_strlen(remainder)] = '\0';
-			// printf("\nbuffer: \"%s\"\n", buffer);
-			// printf("\n\nremainder: \"%s\"\n\n", remainder);
-			return (line);
+			delpos = ft_strnstr(remainder, "\n", ft_strlen(remainder));
+			if (delpos)
+			{
+				line = ft_strndup(remainder, ft_strlen(remainder) - ft_strlen(delpos) + 1);
+				remainder = ft_strndup(delpos + 1, ft_strlen(delpos) - 1);
+				return (line);
+			}
 		}
+		buffer = malloc(BUFFER_SIZE + 1);
+		if (buffer == NULL)
+			return (NULL);
 		bytes = read(fd, buffer, BUFFER_SIZE);
 		if (bytes <= 0)
 		{
 			if (remainder && *remainder)
 			{
-				line = ft_strndup(remainder, ft_strlen(remainder));
+				line = ft_strndup(remainder, ft_strlen(remainder) - ft_strlen(delpos) + 1);
+				free(remainder);
 				remainder = NULL;
 				return (line);
 			}
-			return (NULL);
+			return (free(buffer), NULL);
 		}
 		buffer[bytes] = '\0';
 		remainder = ft_strjoin(remainder, buffer);
 	}
 }
 
-# include <fcntl.h>
-int	main(void)
-{
-	// int		fd = open("/home/dimitris/francinette/tests/get_next_line/gnlTester/files/multiple_line_with_nl", O_RDONLY);
-	int		fd = open("/Users/dchrysov/francinette/tests/get_next_line/gnlTester/files/multiple_line_with_nl", O_RDONLY);
-	char	*s;
-	int		i = 1;
+// # include <fcntl.h>
+// int	main(void)
+// {
+// 	int		fd = open("/home/dimitris/francinette/tests/get_next_line/gnlTester/files/multiple_line_with_nl", O_RDONLY);
+// 	// int		fd = open("/Users/dchrysov/francinette/tests/get_next_line/gnlTester/files/multiple_line_with_nl", O_RDONLY);
+// 	char	*s;
+// 	int		i = 1;
 
-	while ((s = get_next_line(fd)) != NULL)
-		printf("\nline[%d]: \"%s\"\n", i++, s);
-	// printf("\nline[%d]: \"%s\"\n", i++, get_next_line(fd));
-	// printf("\nline[%d]: \"%s\"\n", i++, s);
-	close(fd);
-	return (0);
-}
+// 	while ((s = get_next_line(fd)) != NULL)
+// 		printf("\nline[%d]: \"%s\"\n", i++, s);
+// 	// printf("\nline[%d]: \"%s\"\n", i++, get_next_line(fd));
+// 	// printf("\nline[%d]: \"%s\"\n", i++, s);
+// 	close(fd);
+// 	return (0);
+// }
