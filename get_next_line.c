e3c526bc60cc93eb940ce77982721bc269984cb4
Dimitris Chrysovergis <dchrysov@1-D-13.42heilbronn.de>
2024-10-31 16:10:49 +0100

    get_next_line.c: (n/w) Still not working, will try to redo it in a new file

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dchrysov <dchrysov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 13:32:36 by dchrysov          #+#    #+#             */
-/*   Updated: 2024/10/30 18:10:22 by dchrysov         ###   ########.fr       */
+/*   Updated: 2024/10/31 16:08:46 by dchrysov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,13 @@
 char	*get_next_line(int fd)
 {
 	ssize_t			bytes;
-	char			buffer[BUFFER_SIZE + 1];
+	char			buffer[BUFFER_SIZE + 1];	
 	char			*line;
 	static char		*remainder;
 	char			*delpos;
 
-	if (fd == -1 || BUFFER_SIZE <= 0)
+	line = NULL;
+	if (fd == -1 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
 	while (1)
 	{
@@ -30,12 +31,14 @@ char	*get_next_line(int fd)
 			delpos = ft_strnstr(remainder, "\n", ft_strlen(remainder));
 			if (delpos)
 			{
-				remainder = ft_strndup(delpos + 1, ft_strlen(delpos) - 1);
 				line = ft_strndup(remainder, ft_strlen(remainder) - ft_strlen(delpos)+ 1);
+				free(remainder);
+				remainder = NULL;
+				remainder = ft_strndup(delpos + 1, ft_strlen(delpos) - 1);
+				if (remainder == NULL)
+					return (free(line), NULL);
 				return (line);
 			}
-			free(delpos);
-			delpos = NULL;
 		}
 		bytes = read(fd, buffer, BUFFER_SIZE);
 		if (bytes <= 0)
@@ -49,23 +52,23 @@ char	*get_next_line(int fd)
 			}
 			// free(delpos);
 			// delpos = NULL;
-			return (NULL);
+			return (free(remainder), remainder = NULL, free(line), NULL);
 		}
 		buffer[BUFFER_SIZE] = '\0';
 		remainder = ft_strjoin(remainder, buffer);
 	}
 }
 
-# include <fcntl.h>
+// # include <fcntl.h>
 
-int	main(void)
-{
-	// int		fd = open("/home/dimitris/francinette/tests/get_next_line/gnlTester/files/multiple_line_with_nl", O_RDONLY);
-	int		fd = open("/Users/dchrysov/francinette/tests/get_next_line/fsoares/multiple_nl.txt", O_RDONLY);
-	char	*s;
+// // int	main(void)
+// // {
+// // 	// int		fd = open("/home/dimitris/francinette/tests/get_next_line/gnlTester/files/multiple_line_with_nl", O_RDONLY);
+// // 	int		fd = open("/Users/dchrysov/francinette/tests/get_next_line/fsoares/multiple_nl.txt", O_RDONLY);
+// // 	char	*s;
 
-	while ((s = get_next_line(fd)) != NULL)
-		printf("\"%s\"\n", s);
-	close(fd);
-	return (0);
-}
+// // 	while ((s = get_next_line(fd)) != NULL)
+// // 		printf("\"%s\"\n", s);
+// // 	close(fd);
+// // 	return (0);
+// // }
