a3d81bfefe37ddbe7a27a4dc077eb66506e102d6
Dimitris Chrysovergis <dchrysov@2-C-5.42heilbronn.de>
2024-11-05 20:35:32 +0100

    (n/w): Trying some modifications.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dchrysov <dchrysov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 16:11:31 by dchrysov          #+#    #+#             */
-/*   Updated: 2024/11/05 19:12:28 by dchrysov         ###   ########.fr       */
+/*   Updated: 2024/11/05 20:32:11 by dchrysov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,14 +37,13 @@ static char	*line_found(char **buff, char *del)
 }
 
 /**
- * @brief frees and reallocates sz bytes for the buff and 
- * then assigns it the value of temp + app_str 
+ * @brief frees and then reallocates sz bytes of memory for 
+ * the buff and then assigns the value of temp + app_str to it.
  * 
  * @param buff the buffer to be resized and reassigned.
- * @param sz the size of the buffer to fit the values of
- * both the temp and app_str.
- * @param temp copying of its value to the buffer.
- * @param app_str appending its value to the buffer.
+ * @param sz the allocating size for the buffer.
+ * @param temp copied to the buffer.
+ * @param app_str appended to the buffer.
  * 
  * @returns the buffer variable after resizing and reassigning.
  */
@@ -63,7 +62,35 @@ static char	*buffer_append(char **buff, char *app_str, int sz)
 	ft_strlcpy(*buff, temp, ft_strlen(temp) + 1);
 	ft_strlcat(*buff, app_str, sz + 1);
 	free(temp);
+	temp = NULL;
 	return (*buff);
+}
+
+static char	*delim_found(int fd, char **buff, ssize_t size)
+{
+	ssize_t	bytes;
+	char	*del;
+	char	app_str[BUFFER_SIZE + 1];
+	char	*result;
+
+	del = ft_strchr(*buff, '\n');
+	while (!del)
+	{
+		bytes = read(fd, app_str, BUFFER_SIZE);
+		app_str[bytes] = '\0';
+		if (bytes < 0)
+			return (NULL);
+		if (bytes == 0)
+		{
+			result = ft_strndup(*buff, ft_strlen(*buff));
+			*buff = NULL;
+			return (result);
+		}
+		size += bytes;
+		*buff = buffer_append(buff, app_str, size);
+		del = ft_strchr(*buff, '\n');
+	}
+	return (line_found(&*buff, del));
 }
 
 /**
@@ -85,60 +112,39 @@ char	*get_next_line(int fd)
 	ssize_t		total_size;
 	static char	*buffer;
 	char		to_append[BUFFER_SIZE + 1];
-	char		*line;
-	char		*delpos;
 
 	if (fd == -1 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
 		return (NULL);
 	append_bytes = read(fd, to_append, BUFFER_SIZE);
 	if (append_bytes < 0)
 		return (NULL);
+	to_append[append_bytes] = '\0';
+	total_size = append_bytes;
 	if (append_bytes == 0)
 	{
-		if (!(buffer && *buffer))
+		if (!(buffer && *buffer))											// eof and no buff (last step)
 			return (NULL);
-		else
-		{
-			line = ft_strndup(buffer, ft_strlen(buffer));
-			buffer = NULL;
-			return (line);
-		}
+		else															// eof and still data to the buff (previously to last step)
+			return(delim_found(fd, &buffer, total_size));
 	}
-	total_size = append_bytes;
-	if (buffer != NULL)
-		buffer = buffer_append(&buffer, to_append, total_size);
-	else
+	if (!buffer)														// no eof no data to the buffer (first step)
 		buffer = ft_strndup(to_append, ft_strlen(to_append));
-	delpos = ft_strchr(buffer, '\n');
-	while (!delpos)
-	{
-		append_bytes = read(fd, to_append, BUFFER_SIZE);
-		if (append_bytes < 0)
-			return (NULL);
-		if (append_bytes == 0)
-		{
-			line = ft_strndup(buffer, ft_strlen(buffer));
-			buffer = NULL;
-			return (line);
-		}
-		total_size += append_bytes;
-		buffer = buffer_append(&buffer, to_append, total_size);
-		delpos = ft_strchr(buffer, '\n');
-	}
-	return (line_found(&buffer, delpos));
+	else																// appending to_app to the already not empty buffer
+		buffer = buffer_append(&buffer, to_append, append_bytes);
+	return (delim_found(fd, &buffer, total_size));
 }
 
-// #include <stdio.h>
-// #include <fcntl.h>
-// int	main(void)
-// {
-// 	// int		fd = open("/home/dimitris/francinette/tests/get_next_line/fsoares/lines_around_10.txt", O_RDONLY);
-// 	// int		fd = open("/Users/dchrysov/francinette/tests/get_next_line/fsoares/lines_around_10.txt", O_RDONLY);
-// 	int		fd = open("/Users/dchrysov/francinette/tests/get_next_line/fsoares/variable_nls.txt", O_RDONLY);
-// 	char	*s;
+#include <stdio.h>
+#include <fcntl.h>
+int	main(void)
+{
+	// int		fd = open("/home/dimitris/francinette/tests/get_next_line/fsoares/lines_around_10.txt", O_RDONLY);
+	// int		fd = open("/Users/dchrysov/francinette/tests/get_next_line/fsoares/lines_around_10.txt", O_RDONLY);
+	int		fd = open("/Users/dchrysov/francinette/tests/get_next_line/fsoares/read_error.txt", O_RDONLY);
+	char	*s;
 
-// 	// printf("%s", get_next_line(fd));
-// 	while ((s = get_next_line(fd)) != NULL)
-// 		printf("\"%s\'", s);
-// 	return (0);
-// }
+	// printf("%s", get_next_line(fd));
+	while ((s = get_next_line(fd)) != NULL)
+		printf("\"%s\'", s);
+	return (0);
+}
